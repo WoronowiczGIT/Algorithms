94dc7c5288093c328a82d96a649b974be8a0efba
RaWo87 <woronowicz87@gmail.com>
2019-03-26 14:28:29 +0100

    refreshing structure

--- BubbleSort/src/main/java/SDA/BubbleSorting/BubbleSort.java
+++
@@ -1,6 +0,0 @@
-package SDA.BubbleSorting;
-
-public interface BubbleSort<T> {
-
-    T[] sortByBubbles(T[] tablica);
-}
--- Sortowania/src/main/java/SDA/BubbleSort/BubbleSort.java
+++ Sortowania/src/main/java/SDA/BubbleSort/BubbleSort.java
@@ -1,23 +1,6 @@
-package SDA.BubbleSort;
+package SDA.BubbleSorting;
 
-public class BubbleSort implements Cloneable{
+public interface BubbleSort<T> {
 
-
-    public Integer[] bubbleSortT(Integer[] input) {
-
-            Integer[] resault = input.clone();
-            Integer temp;
-
-            for (int i = 0; i < input.length; i++) {
-                for (int j = 0; j < input.length - 1; j++) {
-                    if (resault[j] > resault[j + 1]) {
-                        temp = resault[j];
-                        resault[j] = resault[j + 1];
-                        resault[j + 1] = temp;
-                    }
-                }
-            }
-            return resault;
-    }
-
+    T[] sortByBubbles(T[] tablica);
 }
