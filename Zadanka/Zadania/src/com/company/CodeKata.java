47ddc55d075e081212fb23714d90fd2647e3b3b1
Rafal Woronowicz <woronowicz87@gmail.com>
2019-06-18 12:32:01 +0200

    remove from array

--- Zadanka/Zadania/src/com/company/CodeKata.java
+++ Zadanka/Zadania/src/com/company/CodeKata.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Random;
+
 public class CodeKata {
     //Ex 1. Count numbers occurrence in an array, and display as Chart, assuming all natural numbers >=0
 
@@ -43,13 +45,46 @@ public class CodeKata {
                 System.out.print(i+" ");
             }
         }
+    }
+    //Ex 2. remove all occurences of given value from array
+    public static int[] valueRemover(int[] array, int value){
+        int occurences = 0;
+        for (int number: array) {
+            if(number == value) occurences++;
+        }
+        int[] result = new int[array.length-occurences];
 
+        int iterator = 0;
+        for (int number : array
+             ) {
+            if(number!=value){
+                result[iterator] = number;
+                iterator++;
+            }
+        }
+        return result;
+}
 
+    public static int[] generateRandomArray(int size){
+
+        int[] result = new int[size];
+        for (int i = 0; i <result.length ; i++) {
+            result[i] = new Random().nextInt(10);
+        }
+        return result;
+    }
+    public static void presentArray(int[] array){
+        for (int number:array) {
+            System.out.print(number+" ");
+        }
     }
 
     public static void main(String[] args) {
+        int[] array = generateRandomArray(50);
+        int[] result = valueRemover(array,5);
+        presentArray(result);
+        System.out.println("\n"+result.length);
 
-        sortAndDisplay(new int[]{1,3,4,8,8,8,3,2});
 
     }
 }
