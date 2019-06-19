0808f67701c00840ba2bdfc6201eb6bc46f3fdcc
Rafal Woronowicz <woronowicz87@gmail.com>
2019-06-19 13:55:09 +0200

    binary search

--- Zadanka/Zadania/src/com/company/BinarySearch.java
+++ Zadanka/Zadania/src/com/company/BinarySearch.java
@@ -6,30 +6,38 @@ import java.time.Instant;
 public class BinarySearch {
     private int[] array;
 
-    public void setArray(int[] array) {
-        this.array = array;
-    }
     public void generateSortedArray(int length, int interval){
         array = new int[length];
         for (int i = 0; i < length; i++) {
-            array[i] = i+interval;
+            array[i] = i*interval;
         }
     }
 
-    public int binarySearch(int value){
+    public void binarySearch(int value){
         Instant binaryStart = Instant.now();
-        int index = -1;
 
+        binary(value,0,array.length-1);
 
-
-        System.out.println(Duration.between(binaryStart,Instant.now()).getSeconds());
-        return index;
+        System.out.println(Duration.between(binaryStart,Instant.now()).getNano());
     }
+
     public void binary(int value, int start, int finish){
+        if(start >= finish){
+            System.out.println("found "+value+" at: "+finish);
+            return;
+        }
+        int pivot = finish - ((finish - start)/2);
+
+        if(value > array[pivot]){
+            binary(value,pivot+1,finish);
+        }else if(value < array[pivot]){
+            binary(value,start,pivot-1);
+        }
 
     }
 
-    public int linearSearch(int value){
+
+    public void linearSearch(int value){
         Instant linearStart = Instant.now();
         int index = -1;
         for (int i = 0; i < array.length; i++) {
@@ -38,13 +46,17 @@ public class BinarySearch {
                 break;
             }
         }
-        System.out.println(Duration.between(linearStart,Instant.now()).getSeconds());
-        return index;
+        System.out.println(Duration.between(linearStart,Instant.now()).getNano()+" at "+ index);
+
     }
 
 
     public static void main(String[] args) {
         BinarySearch bs = new BinarySearch();
-        bs.generateSortedArray(999,3);
+        bs.generateSortedArray(9999999,2);
+        int value = 999999;
+        bs.binarySearch(value);
+        bs.linearSearch(value);
+
     }
 }
