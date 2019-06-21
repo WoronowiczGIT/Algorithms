c381f94af051f8f37288f94370c58213ccaac8f1
Rafal Woronowicz <woronowicz87@gmail.com>
2019-06-21 13:08:49 +0200

    BFS finished

--- Sortowania/src/main/java/SDA/Graphs/BFS/BFS.java
+++ Sortowania/src/main/java/SDA/Graphs/BFS/BFS.java
@@ -1,25 +1,24 @@
 package SDA.Graphs.BFS;
 
 import java.util.*;
-
+// BREADTH FIRST SEARCH
 public class BFS {
 
     List<Node> out = new ArrayList<>();
     Queue<Node> queue = new ArrayDeque<>();
 
     public void search(Node node){
-        if(!node.visited){
-            node.visited = true;
-            out.add(node);
+          // Every node node is not visited at start, and later we add only unvisited
+            node.visited = true; // set to visited
+            out.add(node); // add to result
 
-            for (Node n: node.paths) {
+            for (Node n: node.paths) { // add all unvisited(!) branching paths to queue
                 if(!n.visited){
                     queue.add(n);
                 }
             }
-        }
 
-        if(!queue.isEmpty()){
+            if(!queue.isEmpty()){
             search(queue.poll());
         }
     }
--- Sortowania/src/main/java/SDA/Graphs/BFS/Main.java
+++ Sortowania/src/main/java/SDA/Graphs/BFS/Main.java
@@ -14,18 +14,29 @@ public class Main {
         Node C = new Node("C");
         Node D = new Node("D");
 
+        Node E = new Node("E");
+        Node F = new Node("F");
+        Node G = new Node("G");
+        Node H = new Node("H");
+
         A.paths.add(B);
         A.paths.add(C);
 
-        B.paths.add(B);
-        C.paths.add(D);
-        D.paths.add(A);
+            B.paths.add(B);
+            B.paths.add(E);
+            B.paths.add(G);
+                G.paths.add(H);
+
+            C.paths.add(D);
+            C.paths.add(F);
+
+                D.paths.add(A);
 
         BFS bfs = new BFS();
-        bfs.search(A);
+        bfs.search(D);
         for (Node node: bfs.out
              ) {
-            System.out.println(node.name);
+            System.out.print(node.name+" ");
         }
     }
 }
