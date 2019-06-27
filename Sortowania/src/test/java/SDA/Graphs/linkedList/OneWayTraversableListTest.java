ecd36466c189e63f0417a1cb32bd90f261067a94
Rafal Woronowicz <woronowicz87@gmail.com>
2019-06-27 22:00:42 +0200

    added negative test

--- Sortowania/src/test/java/SDA/Graphs/linkedList/OneWayTraversableListTest.java
+++ Sortowania/src/test/java/SDA/Graphs/linkedList/OneWayTraversableListTest.java
@@ -7,24 +7,33 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class OneWayTraversableListTest {
-    MyList list = new MyList();
-    OneWayTraversableList owtl = new OneWayTraversableList();
+    MyList list;
+    OneWayTraversableList owtl;
+
+    Node nodeA = new Node(1);
+    Node nodeB = new Node(2);
+    Node nodeC = new Node(3);
     @Before
     public void setup(){
-
+        list = new MyList();
+        owtl = new OneWayTraversableList();
     }
-
     @Test
     public void isLooped() {
-        Node nodeA = new Node(1);
-        Node nodeB = new Node(2);
-        Node nodeC = new Node(3);
-
         list.addToList(nodeA);
         list.addToList(nodeB);
         list.addToList(nodeC);
         list.addToList(nodeA);
 
         Assert.assertTrue(owtl.isLooped(nodeA));
+    }
+
+    @Test
+    public void isNotLooped(){
+        list.addToList(nodeA);
+        list.addToList(nodeB);
+        list.addToList(nodeC);
+
+        Assert.assertFalse(owtl.isLooped(nodeA));
     }
 }
